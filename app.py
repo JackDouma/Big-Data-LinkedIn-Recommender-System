a53f27a19db54da00bdaf9ee4a2de971e5e30446
Michael Rosanelli <michaelrosanelli105@gmail.com>
2024-04-09 14:54:22 -0400

    remove applied job from the dataset so it doesnt show up again, and dont let it create recommendations when clicking 'Home'

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 app.secret_key = '1234'
 
 appliedJobs = []
+recommendations = []
 mostRecentSearchTerms = ["","","",""]
+
+refreshRecs = False
 
 # get job data
 jobData = []
@@ -51,6 +54,8 @@ def countMatches(profile, job):
 # route for applying to jobs        
 @app.route('/apply', methods=['POST'])
 def apply():
+    global jobData, refreshRecs
+
     job_title = request.form['job_title']
     job_link = request.form['link']
     job_company = request.form['company']
@@ -59,75 +64,84 @@ def apply():
 
     appliedJobs.append([job_title, job_link, job_company, job_location, job_skills])
 
+    for i in range(len(jobData)):
+        if jobData[i]['job_link'] == job_link:
+            del jobData[i]
+            break
+
     webbrowser.open_new_tab(request.form['link'])
+    refreshRecs = True
     return redirect('/')
 
 # index page
 @app.route('/')
 def index():
+    global refreshRecs, recommendations
     # You might also like... recommendations logic
     # Recommends jobs matching one of the previous search params, but NOT the other two
+    
     title_match_recommendations = []
     skills_match_recommendations = []
     city_match_recommendations = []
     company_match_recommendations = []
-    recommendations = []
     max_recommendations_count = 6
 
-    for job in jobData:
-        title_match = mostRecentSearchTerms[0] in job['job_title'].lower() and mostRecentSearchTerms[0] != ""
-        skills_match = mostRecentSearchTerms[1].lower() in job['job_skills'].lower() and mostRecentSearchTerms[1] != ""
-        city_match = mostRecentSearchTerms[2].lower() in job['job_location'].lower() and mostRecentSearchTerms[2] != ""
-        company_match = mostRecentSearchTerms[3].lower() in job['company'].lower() and mostRecentSearchTerms[3] != ""
+    if refreshRecs:
+        for job in jobData:
+            title_match = mostRecentSearchTerms[0].lower() in job['job_title'].lower() and mostRecentSearchTerms[0] != ""
+            skills_match = mostRecentSearchTerms[1].lower() in job['job_skills'].lower() and mostRecentSearchTerms[1] != ""
+            city_match = mostRecentSearchTerms[2].lower() in job['job_location'].lower() and mostRecentSearchTerms[2] != ""
+            company_match = mostRecentSearchTerms[3].lower() in job['company'].lower() and mostRecentSearchTerms[3] != ""
 
-        if (title_match and not skills_match and not city_match and not company_match):
-            title_match_recommendations.append([job,"Because you searched for \""+mostRecentSearchTerms[0]+"\"."])
+            if (title_match and not skills_match and not city_match and not company_match):
+                title_match_recommendations.append([job,"Because you searched for \""+mostRecentSearchTerms[0]+"\"."])
 
-        elif (skills_match and not title_match and not city_match and not company_match):
-            skills_match_recommendations.append([job,"Because you searched for jobs that required \""+mostRecentSearchTerms[1]+"\"."])
+            elif (skills_match and not title_match and not city_match and not company_match):
+                skills_match_recommendations.append([job,"Because you searched for jobs that required \""+mostRecentSearchTerms[1]+"\"."])
 
-        elif (city_match and not title_match and not skills_match and not company_match):
-            city_match_recommendations.append([job,"Because you searched for jobs in "+mostRecentSearchTerms[2]+"."])
-        
-        elif (company_match and not title_match and not skills_match and not city_match):
-            company_match_recommendations.append([job,"Because you searched for jobs at "+mostRecentSearchTerms[3]+"."])
-        
-        if len(title_match_recommendations) > max_recommendations_count and len(skills_match_recommendations) > max_recommendations_count and len(city_match_recommendations) > max_recommendations_count and len(company_match_recommendations) > max_recommendations_count:
-            break
-
-    # Curate the list of You might also like... recommendations
-    for i in range(max_recommendations_count):
-        if i < len(title_match_recommendations):
-            recommendations.append(title_match_recommendations[i])
-            if len(recommendations) == max_recommendations_count:
-                break
-        if i < len(skills_match_recommendations):
-            recommendations.append(skills_match_recommendations[i])
-            if len(recommendations) == max_recommendations_count:
-                break
-        if i < len(city_match_recommendations):
-            recommendations.append(city_match_recommendations[i])
-            if len(recommendations) == max_recommendations_count:
-                break
-        if i < len(company_match_recommendations):
-            recommendations.append(company_match_recommendations[i])
-            if len(recommendations) == max_recommendations_count:
-                break
+            elif (city_match and not title_match and not skills_match and not company_match):
+                city_match_recommendations.append([job,"Because you searched for jobs in "+mostRecentSearchTerms[2]+"."])
             
-    for profile in randomProfiles:
-        profile_recommendations = []
-        for viewedJob in appliedJobs:
-            for job in jobData:
-                # count the number of matching attributes between the profile and the viewed job
-                matchCount = countMatches(profile, job)
-                # if at least 2 out of 4 attributes match, recommend the job
-                if matchCount >= 2 and job['job_title'] == viewedJob[0]:
-                    profile_recommendations.append(job)
-        recommendations.extend(profile_recommendations)
+            elif (company_match and not title_match and not skills_match and not city_match):
+                company_match_recommendations.append([job,"Because you searched for jobs at "+mostRecentSearchTerms[3]+"."])
+            
+            if len(title_match_recommendations) > max_recommendations_count and len(skills_match_recommendations) > max_recommendations_count and len(city_match_recommendations) > max_recommendations_count and len(company_match_recommendations) > max_recommendations_count:
+                break
 
-    # shuffle and select a maximum of max_recommendations_count recommendations
-    random.shuffle(recommendations)
-    recommendations = recommendations[:max_recommendations_count]
+        # Curate the list of You might also like... recommendations
+        for i in range(max_recommendations_count):
+            if i < len(title_match_recommendations):
+                recommendations.append(title_match_recommendations[i])
+                if len(recommendations) == max_recommendations_count:
+                    break
+            if i < len(skills_match_recommendations):
+                recommendations.append(skills_match_recommendations[i])
+                if len(recommendations) == max_recommendations_count:
+                    break
+            if i < len(city_match_recommendations):
+                recommendations.append(city_match_recommendations[i])
+                if len(recommendations) == max_recommendations_count:
+                    break
+            if i < len(company_match_recommendations):
+                recommendations.append(company_match_recommendations[i])
+                if len(recommendations) == max_recommendations_count:
+                    break
+                
+        for profile in randomProfiles:
+            profile_recommendations = []
+            for viewedJob in appliedJobs:
+                for job in jobData:
+                    # count the number of matching attributes between the profile and the viewed job
+                    matchCount = countMatches(profile, job)
+                    # if at least 2 out of 4 attributes match, recommend the job
+                    if matchCount >= 2 and job['job_title'] == viewedJob[0]:
+                        profile_recommendations.append(job)
+            recommendations.extend(profile_recommendations)
+
+        # shuffle and select a maximum of max_recommendations_count recommendations
+        random.shuffle(recommendations)
+        recommendations = recommendations[:max_recommendations_count]
+        refreshRecs = False
 
     return render_template('index.html', recommendations=recommendations, appliedJobs=appliedJobs)
 
